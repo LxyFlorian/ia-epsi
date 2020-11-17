7ce8c3d8c70b4388ec565ebe48a42a802e34d838
lxyflorian <florianelie8@gmail.com>
2020-11-17 11:39:58 +0100

    remove /dist from .gitignore

---
+++ precache-manifest.6855304b0e9fac5df396917825d36acb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2c3dc3156f21a60e0d",
+    "url": "/ia-epsi/css/app.14adddb2.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/ia-epsi/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e8180761ecb5fbd1cc156f443ffa0eb2",
+    "url": "/ia-epsi/index.html"
+  },
+  {
+    "revision": "d7587c54d3b05c627004",
+    "url": "/ia-epsi/js/about.b034256b.js"
+  },
+  {
+    "revision": "8b2c3dc3156f21a60e0d",
+    "url": "/ia-epsi/js/app.f99fd0a4.js"
+  },
+  {
+    "revision": "7a5c5ea9e5b0dc04cc81",
+    "url": "/ia-epsi/js/chunk-vendors.1d39febe.js"
+  },
+  {
+    "revision": "3fd589e9e9a3583c6a9003a9ba94bf73",
+    "url": "/ia-epsi/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ia-epsi/robots.txt"
+  }
+]);
