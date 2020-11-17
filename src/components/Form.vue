0ab6cdc85a7a96b87155efbc7db8b3a823d55ccc
MGcheck <maxime.guigourez@checkandvisit.com>
2020-11-17 16:49:02 +0100

    Positions absolut for simulation

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,47 +1,47 @@
 <template>
   <div>
   <b-form inline class="form">
-    <div class="Top" style="margin-top: -62%; margin-left: 11.3%">
+    <div class="Top" style="margin-top: -750px; margin-left: 130px">
       <label class= "text" for="inline-form-input-number1">Nombre de Véhicules </label> <br><br><br>
     
       <b-form-input
       id="number1"
       class="mb-2 mr-sm-2 mb-sm-0 cars1"
       type="number"
-      style="width: 5vw; margin-left: -11%; margin-top: -70%"
+      style="width: 100px; margin-left: -20px; margin-top: -100px"
       ></b-form-input>
     </div>
 
-    <div class="Right" style="margin-top: -7.5%; margin-left: 23%">
+    <div class="Right" style="margin-top:  -90px; margin-left: 250px">
       <label class= "text" for="inline-form-input-number1">Nombre de Véhicules</label> <br><br><br>
     
       <b-form-input
       id="number2"
       class="mb-2 mr-sm-2 mb-sm-0 cars2"
       type="number"
-      style="width: 5vw; margin-left: -11%; margin-top: -70%"
+      style="width: 100px; margin-left: -20px; margin-top: -80px"
       ></b-form-input>
     </div>
 
-    <div class="Bot" style="margin-top: 66%; margin-left: -50%">
+    <div class="Bot" style="margin-top: 720px; margin-left: -555px">
       <b-form-input
       id="number2"
       class="mb-2 mr-sm-2 mb-sm-0 cars2"
       type="number"
-      style="width: 5vw; margin-left: -11%; margin-top: -40%"
+      style="width: 100px; margin-left: -20px; margin-top: -40px"
       ></b-form-input>
 
       <label class= "text" for="inline-form-input-number1">Nombre de Véhicules </label> <br><br><br>
     </div>
     
-    <div class="Left" style="margin-top: -7.5%; margin-left: -50%">
+    <div class="Left" style="margin-top: -85px; margin-left: -550px">
       <label class= "text" for="inline-form-input-number1">Nombre de Véhicules </label> <br><br><br>
     
       <b-form-input
       id="number2"
       class="mb-2 mr-sm-2 mb-sm-0 cars2"
       type="number"
-      style="width: 5vw; margin-left: -11%; margin-top: -70%"
+      style="width: 100px; margin-left: -20px; margin-top: -90px"
       ></b-form-input>
     </div>
 
@@ -57,7 +57,7 @@ export default {
 
 <style>
 #app {
-  background: url('~@/assets/carrefour.png') no-repeat center;
+  background: url('~@/assets/carrefour.png') no-repeat 640px 210px;
 }
 .form {
   margin-left: 40%;
