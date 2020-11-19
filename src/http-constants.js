8d0b1972d17c36717beff81dc4e4b535c7dc26fd
CorentinGAutier <cortech12@gmail.com>
2020-11-19 08:31:28 +0100

    get info IA ok

--- src/http-constants.js
+++ src/http-constants.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let baseURL;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-    baseURL = "https://390b5ecad7c4.ngrok.io/";
+    baseURL = "https://c5650f126b98.ngrok.io/";
 } else {
     baseURL = "http://api.example.com";
 }
--- src/views/Video.vue
+++ src/views/Video.vue
@@ -2,64 +2,191 @@
   <div class="vue">
     <h1 class="text-center">Smart Traffic Light</h1>
     <div class="container">
-      <hr>
+      <hr />
       <b-row>
-        <b-col cols="4"><h2>Le moteur de détection <br> utilisé en action </h2></b-col>
-        <b-col cols="8"><iframe class="align-right" width="720" height="480" src="https://www.youtube.com/embed/YcU8YrT-77Q?start=61" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    </b-col>
+        <b-col cols="4"
+          ><h2>
+            Le moteur de détection <br />
+            utilisé en action
+          </h2></b-col
+        >
+        <b-col cols="8"
+          ><iframe
+            class="align-right"
+            width="720"
+            height="480"
+            src="https://www.youtube.com/embed/YcU8YrT-77Q?start=61"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </b-col>
       </b-row>
-      <hr>
+      <hr />
     </div>
     <div class="container mt-5">
       <h2 class="text-center">Notre projet</h2>
       <h3>La problématique</h3>
-        <p class="text-justify">La circulation dans les villes peut être très dense et parfois mal gérée, ce facteur cause des pertes de temps énorme aux gens et une pollution considérable. Pouvons-nous grâce à l’intelligence artificielle aider à fluidifier le trafic de manière intelligente et par conséquent améliorer le temps de trajet d’un grand nombre de citoyens et par la même occasion réduire la pollution? </p>
+      <p class="text-justify">
+        La circulation dans les villes peut être très dense et parfois mal
+        gérée, ce facteur cause des pertes de temps énorme aux gens et une
+        pollution considérable. Pouvons-nous grâce à l’intelligence artificielle
+        aider à fluidifier le trafic de manière intelligente et par conséquent
+        améliorer le temps de trajet d’un grand nombre de citoyens et par la
+        même occasion réduire la pollution?
+      </p>
       <h3>Notre réponse</h3>
-        <p class="text-justify">Sur la route, le temps d’attente au feu peut vite devenir long s’il ne cesse de passer au rouge ou s’il reste au vert trop brièvement. C’est pourquoi nous avons pensé aux feux de route intelligents qui permettent de gérer le flux de circulation à temps réel.</p>
-      <hr>
+      <p class="text-justify">
+        Sur la route, le temps d’attente au feu peut vite devenir long s’il ne
+        cesse de passer au rouge ou s’il reste au vert trop brièvement. C’est
+        pourquoi nous avons pensé aux feux de route intelligents qui permettent
+        de gérer le flux de circulation à temps réel.
+      </p>
+      <hr />
     </div>
-       <div class="container marketing mt-5">
-         <h2 class="text-center">L'équipe</h2>
-        <!-- Three columns of text below the carousel -->
-        <div class="row mt-5">
-          <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <h4>GAUTIER Corentin</h4>
-            <p>I1 Classe 1 DEV</p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <h4>ELIE Florian</h4>
-            <p>I1 Classe 1 DEV</p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <h4>MANSARD Paul</h4>
-            <p>I1 Classe 1 DEV</p>
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
-        <div class="row">
-          <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <h4>LOUETTE Alexis</h4>
-            <p>I1 Classe ASR</p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <h4>GUIGOUREZ Maxime</h4>
-            <p>I1 Classe 2 DEV</p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <h4>BLAIS Morgane</h4>
-            <p>I1 Classe 2 DEV</p>
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
-        <div class="row">
-          <div class="col-lg-4">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <h4>Mfou'ou Tom</h4>
-            <p>I1 Classe 2 DEV</p>
-          </div><!-- /.col-lg-4 -->
+    <div class="container marketing mt-5">
+      <h2 class="text-center">L'équipe</h2>
+      <!-- Three columns of text below the carousel -->
+      <div class="row mt-5">
+        <div class="col-lg-4">
+          <svg
+            class="bd-placeholder-img rounded-circle"
+            width="140"
+            height="140"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: 140x140"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777" />
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+          </svg>
+          <h4>GAUTIER Corentin</h4>
+          <p>I1 Classe 1 DEV</p>
+        </div>
+        <!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <svg
+            class="bd-placeholder-img rounded-circle"
+            width="140"
+            height="140"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: 140x140"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777" />
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+          </svg>
+          <h4>ELIE Florian</h4>
+          <p>I1 Classe 1 DEV</p>
+        </div>
+        <!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <svg
+            class="bd-placeholder-img rounded-circle"
+            width="140"
+            height="140"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: 140x140"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777" />
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+          </svg>
+          <h4>MANSARD Paul</h4>
+          <p>I1 Classe 1 DEV</p>
+        </div>
+        <!-- /.col-lg-4 -->
+      </div>
+      <!-- /.row -->
+      <div class="row">
+        <div class="col-lg-4">
+          <svg
+            class="bd-placeholder-img rounded-circle"
+            width="140"
+            height="140"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: 140x140"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777" />
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+          </svg>
+          <h4>LOUETTE Alexis</h4>
+          <p>I1 Classe ASR</p>
+        </div>
+        <!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <svg
+            class="bd-placeholder-img rounded-circle"
+            width="140"
+            height="140"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: 140x140"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777" />
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+          </svg>
+          <h4>GUIGOUREZ Maxime</h4>
+          <p>I1 Classe 2 DEV</p>
+        </div>
+        <!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <svg
+            class="bd-placeholder-img rounded-circle"
+            width="140"
+            height="140"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: 140x140"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777" />
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+          </svg>
+          <h4>BLAIS Morgane</h4>
+          <p>I1 Classe 2 DEV</p>
+        </div>
+        <!-- /.col-lg-4 -->
+      </div>
+      <!-- /.row -->
+      <div class="row">
+        <div class="col-lg-4">
+          <svg
+            class="bd-placeholder-img rounded-circle"
+            width="140"
+            height="140"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            aria-label="Placeholder: 140x140"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777" />
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+          </svg>
+          <h4>Mfou'ou Tom</h4>
+          <p>I1 Classe 2 DEV</p>
+        </div>
+        <!-- /.col-lg-4 -->
       </div>
     </div>
   </div>
@@ -69,9 +196,9 @@
 #app {
   background: none;
 }
-</style>
 
-h1, h2 {
+h1,
+h2 {
   font-family: Montserrat;
 }
 
@@ -80,5 +207,4 @@ h1 {
   margin: 5% 0%;
   font-size: 4.2em;
 }
-
 </style>
